1104bc7d545855ede2d9aee3ef6858b3075d3ee2
Cristopher Mattheus Lima <mattheuslima0251@gmail.com>
2024-12-23 00:27:16 -0300

    att transition

--- menu.js
+++ menu.js
@@ -1,6 +1,21 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
+let elements = document.querySelectorAll('.hidden')
+
+let myObserver = new IntersectionObserver( (entries) => {
+   entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+        entry.target.classList.add('show')
+       
+    }
+    else {
+           entry.target.classList.remove('show') 
+    }
+   })
+})
+
+elements.forEach((element) => myObserver.observe(element))
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
